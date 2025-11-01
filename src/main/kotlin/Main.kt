68b4316de2daf3bffd3b862d06253a783beb0574
VLADOS637 <vlados637rus@icloud.com>
2025-11-01 14:43:08 +0300

    v1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,37 +2,55 @@ fun fillingMatrix(matrix: Array<IntArray>) {
     for (i in 0 until matrix.size) {
         println("Строка ${i + 1}: ")
         for (j in 0 until matrix[0].size) {
-            val number = readln().toInt()
-            matrix[i][j] = number
+            while (true) {
+                val input = readln().trim()
+                if (input.length == 3 && input.all { it.isDigit() }) {
+                    val number = input.toInt()
+                    matrix[i][j] = number
+                    break
+                } else {
+                    println("Ошибка! Введите ровно три цифры: ")
+                }
+            }
         }
-        println()
     }
 }
 
 fun printMatrix(matrix: Array<IntArray>) {
     for (i in 0 until matrix.size) {
         for (j in 0 until matrix[0].size) {
-            print("${matrix[i][j]} \t")
+            print("${matrix[i][j].toString().padStart(3, '0')}\t")
         }
         println()
     }
 }
 
 fun task1(matrix: Array<IntArray>) : Int {
-    val unique = 0
-    for (i in 0 until matrix.size) {
-        for (j in 0 until matrix[0].size) {
-
+    val uniqueDigits = mutableSetOf<Char>()
+    for (row in matrix) {
+        for (number in row) {
+            number.toString().forEach { digit ->
+                if (digit.isDigit()) {
+                    uniqueDigits.add(digit)
+                }
+            }
         }
     }
 
-    return unique
+    return uniqueDigits.size
 }
 
-fun main(args: Array<String>) {
+fun checkingSimm (matrix: Array<Array<Int>>) : Boolean {
+    return matrix[1][0] == matrix[0][1] &&
+           matrix[2][0] == matrix[0][2] &&
+           matrix[3][0] == matrix[0][3] &&
+           matrix[4][0] == matrix[0][4]
+}
+
+fun main() {
     while (true) {
         println(
-            "1) Task 1.\n" +
+            "1) Программа запрашивает количество строк и столбцов для двухмерного массива. Пользователь вводит необходимое количество трёхзначных чисел (числа могут повторяться). Происходит подсчёт различных цифр в полученном массиве и на консоль выводится двумерный массив из введённых чисел и количество различных цифр использумемых в данном массиве.\n" +
                     "2) Task 2.\n" +
                     "3) Task 3.\n" +
                     "4) Task 4.\n" +
@@ -55,13 +73,32 @@ fun main(args: Array<String>) {
 
                     println("Исходная матрица:")
                     printMatrix(matrix)
+
+                    val unique = task1(matrix)
+                    println("В массиве использовано $unique различных цифр")
                 } else {
                     println("Некорректный ввод: введите положительные целые числа.")
                 }
             }
 
             2 -> {
-
+                val size = 5
+                val matrix: Array<Array<Int>> = Array(size) {
+                    Array(size) {(1 ..9).random() }
+                }
+                println("Исходная матрица:")
+                for (i in 0 until size) {
+                    for (j in 0 until size) {
+                        print("${matrix[i][j]}\t")
+                    }
+                    print("\n")
+                }
+                if (checkingSimm(matrix)) {
+                    println("Матрица является симметричным относительно главной диагонали")
+                }
+                else {
+                    println("Матрица не является симметричной относительно главной диагонали")
+                }
             }
 
             3 -> {
